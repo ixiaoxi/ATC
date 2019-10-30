5c5e8634491b1184b2d53e075a6952f7901540b1
Wei Vera Wang <wvwang@thoughtworks.com>
2019-10-30 16:46:53 +0800

    init project

---
+++ src/com/wv/test/testDemo.java
@@ -0,0 +1,17 @@
+package com.wv.test;
+
+
+public class testDemo {
+	public static void main(String[] args) {
+		String logFile = "YOUR_SPARK_HOME/README.md"; // Should be some file on your system
+		SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+		Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+		long numAs = logData.filter(s -> s.contains("a")).count();
+		long numBs = logData.filter(s -> s.contains("b")).count();
+
+		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+		spark.stop();
+	}
+}
